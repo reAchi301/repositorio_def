2bad2337fe383ba4220deb5496029aacb858238f
reAchi301 <A01707647@tec.mx>
2025-02-27 13:22:36 -0600

    Ya en diferentes archivos

--- routes/plantas.routes.js
+++ routes/plantas.routes.js
@@ -1,44 +1,28 @@
 const express = require('express');
+
 const router = express.Router();
-const html_header = '';
-const html_footer ='';
 
 const plantas = [];
 
 //router.get es para registrar un middleware para peticiones HTTP GET
 router.get('/agregar', (request, response, next) => {
-    response.render('_agregar_planta');
+    response.render('agregar_planta');
 });
+
 //router.post es para registrar un middleware para peticiones HTTP POST
 router.post('/agregar', (request, response, next) => {
     console.log(request.body);
     plantas.push(request.body.nombre);
-    let html = html_header;
-    html += `<div class="columns">`;
-    for(let planta of plantas) {
-        html += `<div class="column">`;
-        html += `<div class="card">
-        <div class="card-content">
-          <div class="content">`;
-        html += planta;
-        html += `</div>
-                </div>
-              </div>
-            </div>`;
-    }
-    html += `</div>`;
-    html += html_footer;
-    response.send(html);
+
+    response.render('lista_plantas', {
+      plantas: plantas,
+    });
 });
 
 const path = require('path');
 
-router.get('/regar',(request, response, next) => {
-  //response.sendFile('../views/index.html');
-   //La ruta a la carpeta views en donde se encontraría el index html, que no tenemos completo
-   response.sendFile(path.join(__dirname,'..', 'views', 'index.html'));
-   //el path join pone la direccion de donde se encuentra el archivo en orden .. , carpeta , nombre
-   //aqui con el path join se une al servidor de tu directorio de nombre
+router.get('/regar', (request, response, next) => {
+  response.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
